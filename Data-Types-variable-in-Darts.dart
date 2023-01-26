8297017a6854b03a8f1a01d6bc7429271c59c9e8
Shazia Ahmed <shaziaahmed2744@gmail.com>
2023-01-26 11:52:31 -0800

    Dart Data Types 7types of varaibles

---
+++ Data-Types-variable-in-Darts.dart
@@ -0,0 +1,41 @@
+main(){
+
+ ///printkarwany k lye ya use hoga 
+ main(){
+  Print();
+ }
+
+///7 data types in dart / varaibles 
+
+  ///local variable
+
+// String 
+String name = 'helo world ';
+  print(name);
+  
+//   int 
+  int age = 10 ;
+  print(age);
+  
+    // Double 
+  double age = 10.10 ;
+  print(age);
+
+// List
+  List arr = [0,1,2,'shazia',4 + 4,5 - 5,6];
+  print (arr);
+  
+//   Booleans
+  bool age= true;
+  print(age);
+
+//   Map
+  Map arr= {'shazia': 'Ahmed','city' : 'karachi'};
+  print(arr['city']);
+  
+//   Dynamic
+  var arr = 10 + 50 ;
+    print(arr);
+
+
+}
